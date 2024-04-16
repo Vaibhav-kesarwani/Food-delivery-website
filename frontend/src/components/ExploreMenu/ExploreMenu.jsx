391705daa1d3e45eda1c75c6e9faeb239a4fea14
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-04-16 21:22:51 +0530

    styled the explore menu

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -14,7 +14,7 @@ const ExploreMenu = () => {
       <div className="explore-menu-list">
         {menu_list.map((item, index) => {
           return (
-            <div key={index} className="explore-menu-list">
+            <div key={index} className="explore-menu-list-item">
               <img src={item.menu_image} />
               <p>{item.menu_name}</p>
             </div>
