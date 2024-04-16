7044346364067737e982e21dfccc50099aed75d9
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-04-16 22:29:05 +0530

    added the state to the image for the catgorisesation in the project

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./ExploreMenu.css";
 import { menu_list } from "../../assets/assets";
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className="explore-menu" id="explore-menu">
       <h1>Explore our menu</h1>
@@ -14,13 +14,25 @@ const ExploreMenu = () => {
       <div className="explore-menu-list">
         {menu_list.map((item, index) => {
           return (
-            <div key={index} className="explore-menu-list-item">
-              <img src={item.menu_image} />
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                )
+              }
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+              />
               <p>{item.menu_name}</p>
             </div>
           );
         })}
       </div>
+      <hr />
     </div>
   );
 };
