496f75ee84a53900fea14c229b91b16f9e208524
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-04-16 21:14:53 +0530

    created the basic structure for the explore menu

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
-import './ExploreMenu.css'
-import { menu_list } from '../../assets/assets';
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
 
 const ExploreMenu = () => {
   return (
-    <div>
-        
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore our menu</h1>
+      <p className="explore-menu-text">
+        Choose from a diverse menu featuring a delectable array of dishes
+        crafted with the finest ingredients and satisfy your cravings and
+        elevate your dining experience, one delicious meal at a time.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div key={index} className="explore-menu-list">
+              <img src={item.menu_image} />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
 
-export default ExploreMenu
+export default ExploreMenu;
