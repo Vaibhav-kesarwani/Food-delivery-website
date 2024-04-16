a2a043ae72e8ef2ed62f95c3118735540f121306
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-04-16 21:08:57 +0530

    made the menu front card -> and file for the exploremenu

---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets';
+
+const ExploreMenu = () => {
+  return (
+    <div>
+        
+    </div>
+  );
+};
+
+export default ExploreMenu
