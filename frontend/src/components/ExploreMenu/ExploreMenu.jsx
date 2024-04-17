6f6e17e2b64e1b3434ce3983d3bdebf889596e3d
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-04-17 10:54:42 +0530

    refactor the code in all the files

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./ExploreMenu.css";
 import { menu_list } from "../../assets/assets";
 
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "./Footer.css";
 import { assets } from "../../assets/assets";
 
