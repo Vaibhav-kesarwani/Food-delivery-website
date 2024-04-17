1b7978c8f5b4354aa73be3b777b4f741bfaf7ed6
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-04-17 12:06:45 +0530

    created the sign up page and the login page

---
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./LoginPopup.css";
+import { assets } from "../../assets/assets";
+
+const LoginPopup = ({ setShowLogin }) => {
+  const [currentState, setCurrentState] = useState("Sign Up");
+
+  return (
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currentState}</h2>
+          <img onClick={() => setShowLogin(false)} src={assets.cross_icon} />
+        </div>
+        <div className="login-popup-input">
+          {currentState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Your name" required />
+          )}
+          <input type="email" placeholder="Your email" required />
+          <input type="password" placeholder="Password" required />
+        </div>
+        <button>
+          {currentState === "Sign Up" ? "Create account" : "Login"}
+        </button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+        {currentState === "Login" ? (
+          <p>
+            Create a new account? <span onClick={() => setCurrentState("Sign Up")}>Click here</span>
+          </p>
+        ) : (
+          <p>
+            Already hav an account? <span onClick={() => setCurrentState("Login")}>Login here</span>
+          </p>
+        )}
+      </form>
+    </div>
+  );
+};
+
+export default LoginPopup;
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css";
 import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
 
   return (
@@ -46,7 +46,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} />
           <div className="dot"></div>
         </div>
-        <button>sign in</button>
+        <button onClick={() => setShowLogin(true)}>sign in</button>
       </div>
     </div>
   );
