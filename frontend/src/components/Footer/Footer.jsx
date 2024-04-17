2239de8a08646e7fc635b1932bdd3f137c589493
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-04-17 10:14:53 +0530

    footer maded

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/assets";
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} />
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. In facere
+            eius illum temporibus, tenetur perferendis repudiandae voluptatum
+            cupiditate ab eligendi numquam sequi velit ipsam nobis?
+          </p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} />
+            <img src={assets.twitter_icon} />
+            <img src={assets.linkedin_icon} />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-343-4567</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 @ Tomato.com - All Right Reserved.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
