b37ec9724fb6867be5a6087dbfd783e6d82f9ba4
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2024-04-21 18:34:27 +0530

    connected the database in it -> all its properties

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/foodRoute.js";
 
 // app config
 const app = express();
@@ -12,6 +13,10 @@ app.use(cors());
 
 // db connection
 connectDB();
+
+// api endpoints
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
 
 app.get("/", (req, res) => {
   res.send("API working");
